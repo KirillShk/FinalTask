cd900ba962af918fbb505cea6fb29054f9e20179
Kirill Shlyakov <Kirill Shlyakov@DESKTOP-NQB6K3O>
2022-11-30 23:42:46 +0300

    Добавьте файлы проекта.

---
+++ FinalTask/MainViewViewModel.cs
@@ -0,0 +1,100 @@
+﻿using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB.Visual;
+using Autodesk.Revit.UI.Selection;
+
+namespace FinalTask
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public DelegateCommand SaveCommand { get; }
+        public string Prefix { get; set; }
+        public string StartNumber { get; set; } 
+        public string Suffix { get; set; }
+        public bool allRooms { get; set; }
+        public bool fewRooms { get; set; }
+        public DelegateCommand Choose { get; }
+
+        public List<Room> rooms { get; set; } = new List<Room>();
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            SaveCommand = new DelegateCommand(OnSelectCommand);
+            Prefix = "";
+            StartNumber ="";
+            Suffix = "";
+            allRooms = true;
+            fewRooms = false;
+            Choose = new DelegateCommand(ChooseRooms);
+        }
+
+        private void ChooseRooms()
+        {
+            RaiseHideRequest();
+            rooms = SelectionUtils.GetOblects(_commandData);
+            RaiseShowRequest();
+        }
+        #region События
+        public event EventHandler CloseRequest;
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler HideRequest;
+        private void RaiseHideRequest()
+        {
+           HideRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler ShowRequest;
+        private void RaiseShowRequest()
+        {
+            ShowRequest?.Invoke(this, EventArgs.Empty);
+        }
+        #endregion 
+        private void OnSelectCommand()
+        {
+            Document arDoc = _commandData.Application.ActiveUIDocument.Document;
+
+            if (allRooms is true)
+            {
+                rooms = SelectionUtils.AllRooms(_commandData);
+            }
+
+            if (rooms.Count == 0)
+            {
+                TaskDialog.Show("Count", "создайте помещения");
+            }
+            string level = arDoc.ActiveView.Name;
+            int i = Convert.ToInt32(StartNumber);
+            if (i <= 0) { TaskDialog.Show("Ошибка", "Порядковый номер д.б. положительный"); }
+            int j;
+            int.TryParse(string.Join("", level.Where(c => char.IsDigit(c))), out j);
+
+            Transaction ts = new Transaction(arDoc);
+            ts.Start("Change Number Rooms");
+            foreach (Room room in rooms)
+            {
+                Parameter number = (Parameter)room.get_Parameter(BuiltInParameter.ROOM_NUMBER);
+                string numberStr = $"{Prefix}-{j}.{i} [{Suffix}]";
+                number.Set(numberStr);
+                i++;
+            }
+            ts.Commit();
+
+            RaiseCloseRequest();
+
+        }
+    }
+}
---
+++ FinalTask/SelectionUtils.cs
@@ -0,0 +1,53 @@
+﻿using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq.Expressions;
+using System.Windows;
+
+namespace FinalTask
+{
+    public class SelectionUtils
+    {
+        public static List<Room> GetOblects(ExternalCommandData commandData, string message = "Выберите элементы")
+        {
+            UIDocument arDoc = commandData.Application.ActiveUIDocument;
+            Document doc = arDoc.Document;
+            var roomList = new List<Room>();
+            try
+            {
+                IList<Reference> selectedElenmentRef = arDoc.Selection.PickObjects(ObjectType.Element, "Выберите помещения");
+                foreach (var selectedElement in selectedElenmentRef)
+                {
+                    Element element = doc.GetElement(selectedElement);
+                    if (element is Room)
+                    {
+                        Room oRoom = (Room)element;
+                        roomList.Add(oRoom);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return roomList;
+        }
+        public static List<Room> AllRooms(ExternalCommandData commandData, string message = "Выберите элементы")
+        {
+            Document arDoc = commandData.Application.ActiveUIDocument.Document;
+            List<Room> rooms = new FilteredElementCollector(arDoc, arDoc.ActiveView.Id)
+                .OfCategory(BuiltInCategory.OST_Rooms)
+                .Cast<Room>()
+                .ToList();
+            return rooms;
+        }
+
+    }
+}
